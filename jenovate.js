249a0d1e6ee70485a257771d212de36f79f6b403
Abhijeetsingh-cell <abhijeetkmr9431@gmail.com>
2026-09-11 15:56:41 +0530

    Refactor playProjectVideo function for clarity

--- jenovate.js
+++ jenovate.js
@@ -1,7 +1,13 @@
+function playProjectVideo() {
+  const container = document.getElementById('mediaSwap');
 
-  function playProjectVideo(){
-    const container = document.getElementById('mediaSwap');
-    container.innerHTML = `
-      <video src="Jenovate-video(1).mp4" controls autoplay playsinline style="width:100%;"></video>
-    `;
-  }
+  container.innerHTML = `
+    <video
+      src="Jenovate-Video(1).mp4"
+      controls
+      autoplay
+      playsinline
+      style="width:100%;">
+    </video>
+  `;
+}
